b99b9ad9264b5dfe43a23e7563acb91c25ef6669
Miguel Cuevas <miguel.cuevas@ufm.edu>
2026-04-25 16:08:03 -0600

    Header Component
    This component has css, html, ts and it has a route for header.

---
+++ proyecto_final_split/frontend/splititeasy/src/app/components/header/header.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class HeaderComponent {
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+}
