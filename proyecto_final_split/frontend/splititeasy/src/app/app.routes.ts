7e44d1e5407a0842bb2a2689e85b9e3bbc10edbf
Miguel Cuevas <miguel.cuevas@ufm.edu>
2026-04-25 19:08:00 -0600

    Upgrade Component
    This component just has HTML

--- proyecto_final_split/frontend/splititeasy/src/app/app.routes.ts
+++ proyecto_final_split/frontend/splititeasy/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header';
+import { UpgradeComponent } from './components/upgrade/upgrade';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'upgrade', component: UpgradeComponent }
+];
