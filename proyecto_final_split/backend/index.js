103dea8bc4cfcb0c15f727f0f8b1ed88c767d040
Jimena Estrada <jimestradaa@gmail.com>
2026-04-29 09:50:22 -0600

    chatbot entity

--- proyecto_final_split/backend/index.js
+++ proyecto_final_split/backend/index.js
@@ -1,17 +1,39 @@
 const express = require('express');
 
 const app = express();
-
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Servidor funcionando correctamente'
-  });
+
+let messages = [];
+
+const generateId = () => {
+  return messages.length > 0
+    ? messages[messages.length - 1].id + 1
+    : 1;
+};
+
+app.post('/chat', (req, res) => {
+  const { userMessage, amount } = req.body;
+
+  const newMessage = {
+    id: generateId(),
+    userMessage,
+    amount,
+    botResponse: "Gasto registrado correctamente",
+    date: new Date()
+  };
+
+  messages.push(newMessage);
+
+  res.status(200).json(newMessage);
 });
-c
+
+app.get('/chat', (req, res) => {
+  res.status(200).json(messages);
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
