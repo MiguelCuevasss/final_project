7d5e7ef8e5d2bd71713b56b08c61810292185390
Miguel Cuevas <miguel.cuevas@ufm.edu>
2026-05-02 18:59:21 -0600

    Chatbot
    I Added backend to angular, I added it to header component. I used html and css.

--- proyecto_final_split/frontend/splititeasy/src/app/components/header/header.ts
+++ proyecto_final_split/frontend/splititeasy/src/app/components/header/header.ts
@@ -1,28 +1,49 @@
 import { Component } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
 import { GreetingPipe } from '../../pipes/greeting-pipe';
-import { DatePipe } from '@angular/common';
+import { ChatService } from '../../services/chat';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, GreetingPipe, DatePipe],
+  imports: [CommonModule, FormsModule, RouterLink, GreetingPipe, DatePipe],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
+
 export class HeaderComponent {
   isMenuOpen = false;
+  today = new Date();
 
-  
+  userMessage: string = '';
+  aiResponse: string = '';
+
+  constructor(private chatService: ChatService) {}
+
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
-    console.log('Menú abierto:', this.isMenuOpen);
   }
 
   closeMenu(): void {
     this.isMenuOpen = false;
   }
 
-  today = new Date();
+  sendMessage(): void {
+  console.log(this.userMessage);
 
+  if (!this.userMessage.trim()) return;
+
+  this.chatService.sendMessage(this.userMessage).subscribe({
+    next: (response) => {
+      this.aiResponse = response.aiResponse;
+    },
+    error: (error) => {
+      console.error(error);
+      this.aiResponse = 'Hubo un error al comunicarse con la IA.';
+    }
+  });
+}
 }
