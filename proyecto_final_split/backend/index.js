d86f0677b4c29a24580a07052c1cbd429c215352
Miguel Cuevas <miguel.cuevas@ufm.edu>
2026-05-07 12:37:51 -0600

    Mongo DB connected

--- proyecto_final_split/backend/index.js
+++ proyecto_final_split/backend/index.js
@@ -4,6 +4,10 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const connectDB = require('./config/db');
+
+connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
